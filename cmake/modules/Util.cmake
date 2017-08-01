2ec7f54a05c7cf42da54604a59c89ee1f5c329ab
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-01 10:05:24 +0100

    add code formatting check function

--- cmake/modules/Util.cmake
+++ cmake/modules/Util.cmake
@@ -30,6 +30,36 @@ function(get_version)
 endfunction()
 
 
+function(format_check)
+  set(options)
+  set(oneValueArgs COMMAND)
+  set(multiValueArgs FILES)
+
+  cmake_parse_arguments(format_check "${options}" "${oneValueArgs}"
+    "${multiValueArgs}" ${ARGN})
+
+  set(rv 0)
+
+  foreach(file ${format_check_FILES})
+    execute_process(COMMAND
+      ${format_check_COMMAND} ${file} | diff -u ${file} - &> /dev/null
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      RESULT_VARIABLE rv
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if(rv)
+      break()
+    endif()
+  endforeach()
+
+  if(rv)
+    message(STATUS "code formatting is compliant")
+  else()
+    message(WARNING "code formatting is not compliant")
+  endif()
+endfunction()
+
+
 function(attach_compilation_db_command trgt)
   if(NOT TARGET ${trgt})
     fatal("cannot attach custom command to non-target: ${trgt}")
