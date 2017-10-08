4dbcbc2c0d86c78a0e7e53ef2e2b89b9ab9252c6
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-10-09 00:07:51 +0100

    impove build scripts

--- utils/scripts/build/build.sh
+++ utils/scripts/build/build.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 
-# initialize configuration vars
-
-SRC_DIR=""
-INSTALL_DIR=""
-
-
 # set configuration vars
 
-if [ -z "$1" ]; then 
-  echo "error: source directory was not provided" 
-
-  exit 1
-fi
+[[ -z $1 ]] && echo "error: source directory was not provided" && exit 1
 
 SRC_DIR=$1
-
-if [ -z "$2" ]; then 
-  INSTALL_DIR="${SRC_DIR}/../install/"
-else
-  INSTALL_DIR="$2"
-fi
-
+INSTALL_DIR=${2:-../install/}
 
 # print configuration vars
 
@@ -31,24 +15,20 @@ echo "info: install dir: ${INSTALL_DIR}"
 echo ""
 
 
-LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -lc++ -lc++abi" 
-
-CC=clang CXX=clang++ \
-  cmake \
+cmake \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
-  -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+  -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+  -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
   -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=On \
   -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
   -DANNOTATELOOPS_SKIP_TESTS=OFF \
   -DANNOTATELOOPS_DEBUG=ON \
   -DBOOST_ROOT=${BOOST_ROOT} \
   -DGTEST_ROOT=${GTEST_ROOT} \
   "${SRC_DIR}"
-
 
 exit $?
 
