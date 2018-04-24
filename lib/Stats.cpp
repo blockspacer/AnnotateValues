877cf901b0a798e2063c5c243a046130ac3a71a8
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-24 18:25:36 +0100

    flesh out file serialization of stats

--- lib/Stats.cpp
+++ lib/Stats.cpp
@@ -4,16 +4,45 @@
 
 #include "Stats.hpp"
 
-#include <cstdlib>
-// using std::abort
+#include "rapidjson/document.h"
+// using rapidjson::Document
 
-#include "nlohmann/json.hpp"
-// using nlohmann::json
+#include "rapidjson/ostreamwrapper.h"
+// using rapidjson::OStreamWrapper
 
-using json = nlohmann::json;
+#include "rapidjson/writer.h"
+// using rapidjson::Writer
+
+#include <fstream>
+// using std::ofstream
+
+namespace json = rapidjson;
 
 namespace icsa {
 
-bool AnnotateInstructionsStats::save(const std::string &Filename) const {}
+bool AnnotateInstructionsStats::save(const std::string &Filename) const {
+  json::Document d;
+  d.SetObject();
+
+  auto &allocator = d.GetAllocator();
+
+  d.AddMember("functions_processed", Functions.size(), allocator);
+
+  json::Value functions(json::kArrayType);
+  for (const auto &e : Functions) {
+    json::Value s{e.c_str(), allocator};
+    functions.PushBack(s, allocator);
+  }
+
+  d.AddMember("functions", functions, allocator);
+
+  std::ofstream ofs(Filename);
+  json::OStreamWrapper osw(ofs);
+  json::Writer<decltype(osw)> writer(osw);
+
+  d.Accept(writer);
+
+  return true;
+}
 
 } // namespace icsa
